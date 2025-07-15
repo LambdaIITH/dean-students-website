06dbaed3df9d6261e863d51108d5e344d9b12790
Pathri Vidya Praveen <vidyapraveen.2006@gmail.com>
2025-07-15 18:53:42 +0530

    Added home page files... Navbar and Footer to be added now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,21 @@
+import React from "react";
+import Image from "next/image";
 
+const Header = () => {
+  return (
+    <header className="bg-white py-2 px-12 sm:px-8 w-full mx-auto flex items-center justify-between shadow-2xl rounded-b-lg">
+      <div className="w-50 h-30 flex-shrink-0 mr-4 flex items-center justify-center">
+        <img
+          src="assets/iith_logo.png"
+          alt="IIT Hyderabad Logo"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <h1 className="flex-grow text-right text-xl sm:text-2xl md:text-3xl font-semibold text-iith-orange-dark">
+        Dean Academics IIT Hyderabad
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white py-3 px-8 w-full shadow-md">
+      <div className="text-[#F58A42] text-center font-medium text-lg">
+        Navbar
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
