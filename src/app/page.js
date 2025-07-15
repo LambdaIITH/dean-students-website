b84039745709fea58e42ee1bedad88744baf8a15
Pathri Vidya Praveen <vidyapraveen.2006@gmail.com>
2025-07-15 21:25:31 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -11,12 +11,12 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-gray-100 font-inter text-gray-900">
   
       
-      <Navbar />
+      
       <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 pt-50">
        
         <HomePageContent />
       </main>
-      <Footer />
+      
     </div>
   );
 }
