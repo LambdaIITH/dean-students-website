9cb48fd7e68cf10808459348265ea6931e0efb7d
nathanalvares19 <nathanalvares2706@gmail.com>
2025-07-16 17:39:44 +0530

    updated sports/fics page

--- src/app/sports/fics/page.js
+++ src/app/sports/fics/page.js
@@ -2,11 +2,11 @@ import Table from "../../../components/sports/fics/Table";
 
 export default function FICsPage() {
   return (
-    <div className="py-8 px-4">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl font-semibold underline text-center mb-6">
+    <section className="py-10">
+      <h1 className="font-bold text-4xl text-center text-[#d1402a] underline underline-offset-8 decoration-[#f58a42] mb-12">
         Faculty In-Charge Details
       </h1>
       <Table />
-    </div>
+    </section>
   );
 }
