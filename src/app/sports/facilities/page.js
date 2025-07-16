6907f1a105574989c2e556345cf336a37e255402
nathanalvares19 <nathanalvares2706@gmail.com>
2025-07-16 17:23:21 +0530

    updated sports/facilities page

--- src/app/sports/facilities/page.js
+++ src/app/sports/facilities/page.js
@@ -1,3 +1,4 @@
+// app/sports/facilities/page.tsx
 import Facility from "../../../components/sports/facilities/Facility";
 
 export default function FacilitiesPage() {
@@ -7,75 +8,38 @@ export default function FacilitiesPage() {
       imgSrc: "/football.jpg",
       title: "Football",
       content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis? In ipsa nemo suscipit doloribus quam soluta fuga odit reprehenderit id aliquam, aliquid cum, provident adipisci molestiae harum maiores commodi alias magnam modi. Assumenda nesciunt atque quibusdam modi rem tempora, quisquam odit, eligendi ab in aliquam temporibus voluptatem, fugiat optio magnam quidem nostrum sapiente vel aperiam quos! Molestias commodi perferendis odit aspernatur magnam quibusdam libero qui expedita quaerat!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis...",
       flip: true,
     },
     {
       id: 2,
       imgSrc: "/football.jpg",
-      title: "Football",
+      title: "Basketball",
       content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis? In ipsa nemo suscipit doloribus quam soluta fuga odit reprehenderit id aliquam, aliquid cum, provident adipisci molestiae harum maiores commodi alias magnam modi. Assumenda nesciunt atque quibusdam modi rem tempora, quisquam odit, eligendi ab in aliquam temporibus voluptatem, fugiat optio magnam quidem nostrum sapiente vel aperiam quos! Molestias commodi perferendis odit aspernatur magnam quibusdam libero qui expedita quaerat!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis...",
       flip: false,
     },
     {
       id: 3,
       imgSrc: "/football.jpg",
-      title: "Football",
+      title: "Tennis",
       content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis? In ipsa nemo suscipit doloribus quam soluta fuga odit reprehenderit id aliquam, aliquid cum, provident adipisci molestiae harum maiores commodi alias magnam modi. Assumenda nesciunt atque quibusdam modi rem tempora, quisquam odit, eligendi ab in aliquam temporibus voluptatem, fugiat optio magnam quidem nostrum sapiente vel aperiam quos! Molestias commodi perferendis odit aspernatur magnam quibusdam libero qui expedita quaerat!",
-      flip: false,
-    },
-    {
-      id: 4,
-      imgSrc: "/football.jpg",
-      title: "Football",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis? In ipsa nemo suscipit doloribus quam soluta fuga odit reprehenderit id aliquam, aliquid cum, provident adipisci molestiae harum maiores commodi alias magnam modi. Assumenda nesciunt atque quibusdam modi rem tempora, quisquam odit, eligendi ab in aliquam temporibus voluptatem, fugiat optio magnam quidem nostrum sapiente vel aperiam quos! Molestias commodi perferendis odit aspernatur magnam quibusdam libero qui expedita quaerat!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis...",
       flip: true,
     },
-    {
-      id: 5,
-      imgSrc: "/football.jpg",
-      title: "Football",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis? In ipsa nemo suscipit doloribus quam soluta fuga odit reprehenderit id aliquam, aliquid cum, provident adipisci molestiae harum maiores commodi alias magnam modi. Assumenda nesciunt atque quibusdam modi rem tempora, quisquam odit, eligendi ab in aliquam temporibus voluptatem, fugiat optio magnam quidem nostrum sapiente vel aperiam quos! Molestias commodi perferendis odit aspernatur magnam quibusdam libero qui expedita quaerat!",
-      flip: false,
-    },
-    {
-      id: 6,
-      imgSrc: "/football.jpg",
-      title: "Football",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis? In ipsa nemo suscipit doloribus quam soluta fuga odit reprehenderit id aliquam, aliquid cum, provident adipisci molestiae harum maiores commodi alias magnam modi. Assumenda nesciunt atque quibusdam modi rem tempora, quisquam odit, eligendi ab in aliquam temporibus voluptatem, fugiat optio magnam quidem nostrum sapiente vel aperiam quos! Molestias commodi perferendis odit aspernatur magnam quibusdam libero qui expedita quaerat!",
-      flip: true,
-    },
-    {
-      id: 7,
-      imgSrc: "/football.jpg",
-      title: "Football",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus sunt consequatur omnis? In ipsa nemo suscipit doloribus quam soluta fuga odit reprehenderit id aliquam, aliquid cum, provident adipisci molestiae harum maiores commodi alias magnam modi. Assumenda nesciunt atque quibusdam modi rem tempora, quisquam odit, eligendi ab in aliquam temporibus voluptatem, fugiat optio magnam quidem nostrum sapiente vel aperiam quos! Molestias commodi perferendis odit aspernatur magnam quibusdam libero qui expedita quaerat!",
-      flip: true,
-    },
+    // Add more facilities as needed
   ];
 
   return (
-    <div className="flex flex-col gap-2 py-5">
-      <h1 className="font-semibold px-4 text-[3rem] text-center underline mb-6">
+    <section className="py-10">
+      <h1 className="font-bold text-4xl text-center text-[#d1402a] underline underline-offset-8 decoration-[#f58a42] mb-12">
         Sports Facilities
       </h1>
-      {facilitiesList.map((facility) => {
-        return (
-          <Facility
-            key={facility.id}
-            imgSrc={facility.imgSrc}
-            title={facility.title}
-            content={facility.content}
-            flip={facility.flip}
-          />
-        );
-      })}
-    </div>
+      <div className="flex flex-col gap-6">
+        {facilitiesList.map((facility) => (
+          <Facility key={facility.id} {...facility} />
+        ))}
+      </div>
+    </section>
   );
 }
