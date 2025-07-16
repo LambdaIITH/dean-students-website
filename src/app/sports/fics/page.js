d6e152254adf3c6c6602fdb4cdb936ae917f0dd1
nathanalvares19 <nathanalvares2706@gmail.com>
2025-07-16 18:14:24 +0530

    created student-activities/fics page

--- src/app/sports/fics/page.js
+++ src/app/sports/fics/page.js
@@ -4,7 +4,7 @@ export default function FICsPage() {
   return (
     <section className="py-10">
       <h1 className="font-bold text-4xl text-center text-[#d1402a] underline underline-offset-8 decoration-[#f58a42] mb-12">
-        Faculty In-Charge Details
+        Faculties In-Charge {"(Sports)"}
       </h1>
       <Table />
     </section>
--- src/app/student-activities/fics/page.js
+++ src/app/student-activities/fics/page.js
@@ -1,3 +1,12 @@
+import Table from "../../../components/sports/fics/Table";
+
 export default function FICsPage() {
-  return <div>FICS for student gymkhana</div>;
+  return (
+    <section className="py-10">
+      <h1 className="font-bold text-4xl text-center text-[#d1402a] underline underline-offset-8 decoration-[#f58a42] mb-12">
+        Faculties In-Charge {"(Student Activities)"}
+      </h1>
+      <Table />
+    </section>
+  );
 }
