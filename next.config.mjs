c1cfe9cea9f7cb67f566a8f35cb2340aecb83c55
Pathri Vidya Praveen <vidyapraveen.2006@gmail.com>
2025-07-16 18:03:54 +0530

    Fixed next.config.mjs file which I had messed up in merging

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-// next.config.mjs
-const nextConfig = {
-  webpack(config, { dev, isServer }) {
-    if (dev && !isServer) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
